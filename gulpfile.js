ed0e782ef65ebab86865021663a6c5bf1f0be352
Brent Jackson <jacksonblack@gmail.com>
2014-09-28 14:36:19 -0400

    Clean up package

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,6 @@
 
 var gulp = require('gulp');
 var browserify = require('gulp-browserify');
-var rename = require('gulp-rename');
 var uglify = require('gulp-uglify');
 var browserSync = require('browser-sync');
 var mincss = require('gulp-minify-css');
